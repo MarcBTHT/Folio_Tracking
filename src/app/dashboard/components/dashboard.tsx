e1c76caa694fd85968af41d0b6bc99c9a54a8644
MarcBTHT <marc.berthol29@gmail.com>
2024-01-01 19:53:17 +0100

    Viem + synchronizer button

--- src/app/dashboard/components/dashboard.tsx
+++ src/app/dashboard/components/dashboard.tsx
@@ -5,7 +5,9 @@ import { useAccount, useBalance, useContractReads, useNetwork } from 'wagmi'
 import { erc20ABIperso } from './constants';
 import { formatUnits } from 'viem'
 
-import tokenList from './tokenList.json';
+//import tokenList from './tokenList.json';
+import tokenList from './tokenListTest.json';
+//import tokenList from './tokenListAave.json';
 import { config } from '../../../wagmi';
 
 export function Dashboard() {
@@ -31,6 +33,7 @@ interface TokenInfo {
 export function GetAccountBalance() {
     const { address } = useAccount();
     const [tokenInfo, setTokenInfo] = useState<TokenInfo>({});
+    const [isSyncing, setIsSyncing] = useState(false);
 
     useEffect(() => {
         async function fetchBalances() {
@@ -47,54 +50,67 @@ export function GetAccountBalance() {
                         });
 
                         const balance = data && formatUnits(data, token.decimals);
-
-                        newTokenInfo[token.address] = {
-                            name: token.name,
-                            symbol: token.symbol,
-                            decimals: token.decimals,
-                            logoURI: token.logoURI,
-                            balance: balance || '0'
-                        };
-
-                    } catch (error) {}
+                        
+                        if (balance && balance != '0') {
+                            newTokenInfo[token.address] = {
+                                name: token.name,
+                                symbol: token.symbol,
+                                decimals: token.decimals,
+                                logoURI: token.logoURI,
+                                balance: balance || '0'
+                            };
+                        }
+                    } catch (error) { }
                 }
                 setTokenInfo(newTokenInfo);
+                setIsSyncing(false);
             }
         }
         fetchBalances();
-    }, [address]);
+    }, [address, isSyncing]);
+
+    const handleSyncClick = () => {
+        setIsSyncing(true); 
+    };
 
     return (
         <div className="mt-4 mx-auto w-3/4 bg-white shadow-lg rounded-lg p-6">
-            <div className="overflow-x-auto">
-                <table className="w-full text-left">
-                    <thead>
-                        <tr>
-                            <th className="pb-4">ACTIFS</th>
-                            <th className="pb-4">MONTANT</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr className="border-b">
-                            <GetChainBalance />
-                        </tr>
-                        {Object.entries(tokenInfo).map(([address, { balance, name, symbol, logoURI }]) => (
-                            <tr key={address} className="border-b">
-                                <td className="py-4">
-                                    <div className="flex items-center">
-                                        <img className="h-6 w-6 rounded-full mr-2" src={logoURI} alt={name} />
-                                        <span>{name} ({symbol})</span>
-                                    </div>
-                                </td>
-                                <td className="py-4">{balance} {symbol}</td>
+            <button onClick={handleSyncClick} disabled={isSyncing} className="bg-customOrange hover:bg-customPink text-white font-bold py-2 px-4 rounded mb-4">
+                {isSyncing ? 'Synchronisation en cours...' : 'Synchroniser'}
+            </button>
+            {isSyncing ? (
+                <p></p>
+            ) : (
+                <div className="overflow-x-auto">
+                    <table className="w-full text-left">
+                        <thead>
+                            <tr>
+                                <th className="pb-4">ACTIFS</th>
+                                <th className="pb-4">MONTANT</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+                        </thead>
+                        <tbody>
+                            <tr className="border-b">
+                                <GetChainBalance />
+                            </tr>
+                            {Object.entries(tokenInfo).map(([address, { balance, name, symbol, logoURI }]) => (
+                                <tr key={address} className="border-b">
+                                    <td className="py-4">
+                                        <div className="flex items-center">
+                                            <img className="h-6 w-6 rounded-full mr-2" src={logoURI} alt={name} />
+                                            <span>{name} ({symbol})</span>
+                                        </div>
+                                    </td>
+                                    <td className="py-4">{balance} {symbol}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+             )}
         </div>
     );
-    
+
 }
 
 export function GetChainBalance() { //I can put this function in GetAccountBalance (I just wait to see how I will handle the rest)
